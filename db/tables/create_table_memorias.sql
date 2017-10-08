88eeed395228ee657087f8292d89ebb8d6f8bd48
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 20:09:50 -0300

    Update create_table_memorias.sql

--- db/tables/create_table_memorias.sql
+++ db/tables/create_table_memorias.sql
@@ -1,8 +1,8 @@
-create table memorias(
-id_unico integer,
-tamanho integer,
-clock integer,
-sockete varchar(10),
-PRIMARY KEY(id_unico),
-foreign key(id_unico) references registros_de_entrada(id_unico)
-);
+CREATE TABLE `memorias` (
+  `id_unico` int(11) NOT NULL,
+  `tamanho` int(11) DEFAULT NULL,
+  `clock` int(11) DEFAULT NULL,
+  `sockete` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id_unico`),
+  FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
+)
