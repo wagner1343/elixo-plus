82b29e3676482a1f06a1be74d49e1f6b6e9402be
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 20:29:27 -0300

    Update create_table_memorias.sql

--- db/tables/create_table_memorias.sql
+++ db/tables/create_table_memorias.sql
@@ -4,5 +4,5 @@ CREATE TABLE `memorias` (
   `clock` int(11) DEFAULT NULL,
   `sockete` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id_unico`),
-  FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
-)
+  CONSTRAINT `memorias_ibfk_1` FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
