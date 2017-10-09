eaf6ede666a55a7313bd789eb1070a9af48a1725
Wagner Silvestre <wagner1343@outlook.com>
2017-10-09 01:39:31 -0300

    Deleted untracked files

--- db/tables/create_table_registro_de_entrada.sql
+++
@@ -1,5 +0,0 @@
-create table registros_de_entrada(
-  id_unico int not null primary key auto_increment,
-  descricao varchar(100) not null,
-  data datetime not null,
-  usuario varchar(30) not null);
