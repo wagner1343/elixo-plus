c372956edbd13a9386cc19735fbf097e97ff0e0c
GabrielKrysa <gabrielkrysa@gmail.com>
2017-10-08 21:56:19 -0300

    Update create_table_registros_de_entrada.sql

--- db/tables/create_table_registros_de_entrada.sql
+++ db/tables/create_table_registros_de_entrada.sql
@@ -3,5 +3,5 @@ CREATE TABLE `registros_de_entrada` (
   `descricao` varchar(100) DEFAULT NULL,
   `data` datetime NOT NULL,
   `usuario` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id_unico`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_unico`));
+
