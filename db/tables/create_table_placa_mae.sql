a2a4e47024bfd6ea571804be7444f4025c4a8038
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 23:10:32 -0300

    Update create_table_placa_mae.sql

--- db/tables/create_table_placa_mae.sql
+++ db/tables/create_table_placa_mae.sql
@@ -1,6 +1,6 @@
 create table placa_mae(
-	id_mae int primary key not null auto_increment,
+	id int primary key not null auto_increment,
 	ddr int not null,
 	suporte_ide int not null,
 	suporte_sata int not null,
-	foreign key id3(id_mae) references registros_de_entrada(id_unico));
+	foreign key id3(id) references registros_de_entrada(id_unico));
