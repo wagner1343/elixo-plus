172510cea4eda34cdcc585967a14ac97de2f4c7f
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 22:28:46 -0300

    Delete create_table_memorias.sql

--- db/tables/create_table_memorias.sql
+++
@@ -1,6 +0,0 @@
-create table memoria(
-id_memoria int primary key not null ,
-tamanho int not null,
-clock int not null,
-sockete varchar(10) not null,
-foreign key id2(id_memoria) references registros_de_entrada(id_unico));
