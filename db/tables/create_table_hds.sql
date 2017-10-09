541045d876b462ac0a9bea7e0842a430f50a0f3b
GabrielKrysa <gabrielkrysa@gmail.com>
2017-10-08 21:00:10 -0300

    Update create_table_hds.sql

--- db/tables/create_table_hds.sql
+++ db/tables/create_table_hds.sql
@@ -1,15 +1,4 @@
 CREATE TABLE `hds` (
-  `id_unico` int(11) NOT NULL,
-  `tamanho` int(11) DEFAULT NULL,
-  `rpm` int(11) DEFAULT NULL,
-  `interface` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id_unico`),
-  CONSTRAINT `hds_ibfk_1` FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
----------------------------------------------------------------------------------------------------------------------------------
-REATE TABLE `hds` (
   `id_hd` int(11) NOT NULL AUTO_INCREMENT,
   `tamanho` int(11) NOT NULL,
   `rpm` int(11) NOT NULL,
