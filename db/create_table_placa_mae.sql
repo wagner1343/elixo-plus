fdcc22061ef67781e0ef9d193e4923c1ad47750b
GabrielKrysa <gabrielkrysa@gmail.com>
2017-10-08 22:54:22 -0300

    Delete create_table_placa_mae.sql

--- db/create_table_placa_mae.sql
+++
@@ -1,6 +0,0 @@
-create table placa_mae(
-	id_mae int primary key not null auto_increment,
-	ddr int not null,
-	suporte_ide int not null,
-	suporte_sata int not null,
-	foreign key id3(id_mae) references registros_de_entrada(id_unico));
