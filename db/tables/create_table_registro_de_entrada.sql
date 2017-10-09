225cd72f43ac79f080c24da55592f3380b0c5dbe
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 23:30:21 -0300

    Update create_table_registro_de_entrada.sql

--- db/tables/create_table_registro_de_entrada.sql
+++ db/tables/create_table_registro_de_entrada.sql
@@ -1,5 +1,5 @@
 create table registro_de_entrada(
-  id_unico int(11) not null auto_increment primary key,
+  id_unico int(11) not null primary key,
   descricao varchar(100) not null,
   data datetime not null,
   usuario varchar(30) not null);
