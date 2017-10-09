3925d68235a030f0805ba3fbde08f02b98f0e35d
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 22:28:37 -0300

    Delete create_table_hds.sql

--- db/tables/create_table_hds.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `hds` (
-  `id_hd` int(11) NOT NULL AUTO_INCREMENT,
-  `tamanho` int(11) NOT NULL,
-  `rpm` int(11) NOT NULL,
-  `conexão` varchar(10) NOT NULL,
-  `fabricante` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_hd`),
-  CONSTRAINT `id` FOREIGN KEY (`id_hd`) REFERENCES `id_unico` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 
