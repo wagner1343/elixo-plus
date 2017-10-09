930d1a50d5aeaf6e8d7d50463b18780146db58a7
GabrielKrysa <gabrielkrysa@gmail.com>
2017-10-08 21:01:24 -0300

    Update create_table_memorias.sql

--- db/tables/create_table_memorias.sql
+++ db/tables/create_table_memorias.sql
@@ -7,12 +7,4 @@ CREATE TABLE `memorias` (
   CONSTRAINT `memorias_ibfk_1` FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--------------------------------------------------------------------------------------------------------------------------------
-memoria | CREATE TABLE `memoria` (
-  `id_memoria` int(11) NOT NULL AUTO_INCREMENT,
-  `tamanho` int(11) NOT NULL,
-  `clock` int(11) NOT NULL,
-  `sockete` int(11) NOT NULL,
-  PRIMARY KEY (`id_memoria`),
-  CONSTRAINT `id3` FOREIGN KEY (`id_memoria`) REFERENCES `id_unico` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
