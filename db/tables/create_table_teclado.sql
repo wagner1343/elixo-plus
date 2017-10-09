3efd7a302a514c09fa5537b0f2872fe0166b6cd7
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 22:29:33 -0300

    Delete create_table_teclado.sql

--- db/tables/create_table_teclado.sql
+++
@@ -1,7 +0,0 @@
-teclado | CREATE TABLE `teclado` (
-  `id_teclado` int(11) NOT NULL AUTO_INCREMENT,
-  `fabricante` varchar(20) DEFAULT NULL,
-  `conexão` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id_teclado`),
-  CONSTRAINT `id7` FOREIGN KEY (`id_teclado`) REFERENCES `id_unico` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
