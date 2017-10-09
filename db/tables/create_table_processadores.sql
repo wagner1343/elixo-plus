8d067163a4b603794c13f1b7a62c69fc4adae534
GabrielKrysa <gabrielkrysa@gmail.com>
2017-10-08 21:01:42 -0300

    Update create_table_processadores.sql

--- db/tables/create_table_processadores.sql
+++ db/tables/create_table_processadores.sql
@@ -1,15 +1,4 @@
-CREATE TABLE `processadores` (
-  `id_unico` int(11) NOT NULL,
-  `clock` int(11) DEFAULT NULL,
-  `sockete` varchar(10) DEFAULT NULL,
-  `nucleos` int(11) DEFAULT NULL,
-  `modelo` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id_unico`),
-  CONSTRAINT `processadores_ibfk_1` FOREIGN KEY (`id_unico`) REFERENCES `registros_de_entrada` (`id_unico`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-----------------------------------------------------------------------------------------------------------------------------------
- processador | CREATE TABLE `processador` (
+CREATE TABLE `processador` (
   `id_processador` int(11) NOT NULL AUTO_INCREMENT,
   `modelo` varchar(30) DEFAULT NULL,
   `clock` float NOT NULL,
