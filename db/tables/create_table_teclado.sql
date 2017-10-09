72201225813e227958c265a659a8af1128033b83
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 23:30:12 -0300

    Update create_table_teclado.sql

--- db/tables/create_table_teclado.sql
+++ db/tables/create_table_teclado.sql
@@ -1,5 +1,5 @@
 create table teclado(
-	id int primary key auto_increment,
+	id int primary key,
 	fabricante varchar(20),
 	sockete varchar(10),
 	foreign key id6(id) references registros_de_entrada(id_unico));
