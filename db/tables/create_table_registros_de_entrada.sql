f17d642d776dc264970582fc17d3c3d97ebbf1ec
Wagner Silvestre <wagner1343@outlook.com>
2017-10-09 01:26:18 -0300

    Renamed create_table_registro_de_entrada

---
+++ db/tables/create_table_registros_de_entrada.sql
@@ -0,0 +1,5 @@
+create table registros_de_entrada(
+  id_unico int not null primary key auto_increment,
+  descricao varchar(100) not null,
+  data datetime not null,
+  usuario varchar(30) not null);
