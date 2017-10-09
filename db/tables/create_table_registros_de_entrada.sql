a8f2d53847f851fadef547de141b6f84ff236df5
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 22:29:26 -0300

    Delete create_table_registros_de_entrada.sql

--- db/tables/create_table_registros_de_entrada.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `registros_de_entrada` (
-  `id_unico` int(11) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(100) DEFAULT NULL,
-  `data` datetime NOT NULL,
-  `usuario` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id_unico`));
-
