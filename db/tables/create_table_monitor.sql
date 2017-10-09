6c5f4713b4e42446963e0d1c571a6ade52c27f75
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 23:30:48 -0300

    Update create_table_monitor.sql

--- db/tables/create_table_monitor.sql
+++ db/tables/create_table_monitor.sql
@@ -1,5 +1,5 @@
 create table monitor(
-	id int primary key auto_increment,
+	id int primary key,
 	tamanho int,
 	fabricante varchar(20),
 	foreign key id4(id) references registros_de_entrada(id_unico));
