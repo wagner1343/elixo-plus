f9a085eb3a191ac90f2c7058873c7d698e26890b
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 23:37:48 -0300

    Update create_table_mouse.sql

--- db/tables/create_table_mouse.sql
+++ db/tables/create_table_mouse.sql
@@ -2,4 +2,4 @@ create table mouse(
 	id int primary key ,
 	fabricante varchar(20),
 	sockete varchar(10),
-	foreign key id5(id) references registro_de_entrada(id_unico));
+	foreign key id5(id) references registros_de_entrada(id_unico));
