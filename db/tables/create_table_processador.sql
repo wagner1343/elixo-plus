5616749261742945f2b556bc686acafc0605edc1
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2017-10-08 23:13:07 -0300

    Update create_table_processador.sql

--- db/tables/create_table_processador.sql
+++ db/tables/create_table_processador.sql
@@ -1,7 +1,7 @@
 create table processador(
-	id_processador int primary key,
+	id int primary key,
 	modelo varchar (30),
 	clock float(11) not null,
 	sockete varchar(10) not null,
 	nucleos int not null,
-	foreign key id1(id_processador) references registros_de_entrada(id_unico));
+	foreign key id1(id) references registros_de_entrada(id_unico));
